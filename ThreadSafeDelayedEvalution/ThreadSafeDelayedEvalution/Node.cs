c7b1c10b008f5c3b31165e3ed9ff43359bbc8711
Christoph Mayerhofer <christoph.julian.mayerhofer@gmail.com>
2022-06-12 21:30:59 +0200

    added simple implementation for thread safe delayed evaluation with destructor missing that clears all parents from his own notifications

--- ThreadSafeDelayedEvalution/ThreadSafeDelayedEvalution/Node.cs
+++ ThreadSafeDelayedEvalution/ThreadSafeDelayedEvalution/Node.cs
@@ -1,71 +1,135 @@
-﻿namespace ThreadSafeDelayedEvalution;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Node
+namespace ThreadSafeDelayedEvalution
 {
-
-    private bool _dirty = false;
-    private int _param = 0;
-    private Node? _parent = null;
-    private bool _enabled = true;
-    private List<Node> _observerList = new List<Node>();
-    ~Node()
+    public class Node
     {
-        throw new Exception("not implemented");
 
-    }
+        private bool _dirty = false;
+        private int _param = 0;
+        private Node _parent = null;
+        private bool _enabled = true;
+        private int _result = 0;
+        private List<Node> _observerList = new();
 
-    public void addReference()
-    {
-        throw new Exception("not implemented");
+        private uint _refCounter = 0;
 
-    }
 
-    public void releaseReference()
-    {
-        throw new Exception("not implemented");
-
-    }
-
-    public void setParent(Node parent)
-    {
-        _parent = parent;
-        setDirty();
-    }
-
-    public void setParam(int param)
-    {
-        throw new Exception("not implemented");
-
-    }
-    // Return the sum of the parameter and the result of the parent:
-    public int getResult()
-    {
-        //maybe this is wrong?
-        int result = 0;
-
-        if (_dirty)
+        private void CustomDeStructor()
         {
-            _dirty = false;
-            result = 0;
-            if (_enabled)
+            throw new Exception("not implemented");
+
+            //TODO: Clear all parent nodes from my node
+        }
+
+
+        private void checkIfValidNode()
+        {
+            if (_observerList.Count == 0 && _refCounter == 0)
             {
-                result = _param;
-                if (_parent != null)
-                {
-                    result = result += _parent.getResult();
-                    _parent.registerNotification(this);
-                }
+                throw new AccessViolationException("This node is not valid and should never be called :(");
             }
         }
-        return result;
-    }
 
-    public void setDirty()
-    {
-        _dirty = true;
-        observerscopy =;
-        oservers = emptylist;
-        for (observer in observerscopy)
-        observer->setDirty();
+
+
+        public void  addReference()
+        {
+
+            //node is always invalid with this example code=?
+            //checkIfValidNode();
+            
+            _refCounter++;
+        }
+
+        public void releaseReference()
+        {
+            checkIfValidNode();
+
+            _refCounter--;
+            if (_refCounter == 0 && _observerList.Count == 0)
+            {
+
+                CustomDeStructor();
+            }
+
+        }
+
+        public void setParent(Node parent)
+        {
+            checkIfValidNode();
+
+            _parent = parent;
+
+            _parent.registerNotification(this);
+            setDirty();
+        }
+
+
+
+/*
+        public void setEnabled(bool enabled)
+        {
+            _enabled = enabled;
+            setDirty();
+        }*/
+
+        public void setParam(int param)
+        {
+            checkIfValidNode();
+            _param = param;
+            setDirty();
+        }
+
+        // Return the sum of the parameter and the result of the parent:
+        public int getResult()
+        {
+            checkIfValidNode();
+
+            if (_dirty)
+            {
+                _dirty = false;
+                _result = 0;
+                if (_enabled)
+                {
+                    _result = _param;
+                    if (_parent != null)
+                    {
+                        _result += _parent.getResult();
+                        _parent.registerNotification(this);
+                    }
+                }
+            }
+            return _result;
+        }
+
+        public void setDirty()
+        {
+            checkIfValidNode();
+
+            _dirty = true;
+
+            foreach (var observer in _observerList)
+            {
+                observer.setDirty();
+            }
+
+            //TODO:: Clear i echt die Observer wenn is dirty gsetzt hab=?
+
+            _observerList.Clear();
+        }
+
+
+        void registerNotification(Node depNode)
+        {
+            checkIfValidNode();
+
+            if (!_observerList.Contains(depNode))
+            {
+                _observerList.Add(depNode);
+            }
+        }
     }
-};
+}
